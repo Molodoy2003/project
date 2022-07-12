2a2ebc5e8c529624f64b4ec98b2eeba6d2950eb1
Danik <danik.bobko@bk.ru>
2022-07-12 14:04:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+"use strict";
+
+// КУРСЫ
+
+
+
+// ОПЕРАТОРЫ.  ИНКРЕМЕНТ И ДЕКРЕМЕНТ (ПРЕФИКСНЫЙ И ПОСТФИКСТНЫЙ)
+
+
+let incr = 10,
+	 decr = 10;
+
+console.log(incr++);  // ответ 10
+console.log(decr--);  // ответ 10
+
+console.log(++incr);  // ответ 11
+console.log(--decr);  // ответ 9
+
+// && - оператор и
+// || - оператор или 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ОБЪЕКТЫ
+
+
+// let people = {
+// 	name: 'Danik',
+// 	age: 19,
+// 	city: 'Brest',
+// }
+// people.street = 'Lenina'; // добавление элемента
+// delete people.street;     // удаление элемента
+
+
+
+
+
+
+// ФУНКЦИИ 
+
+
+// function bark(name, weight) {
+// 	if (weight > 20) {
+// 		console.log(name + ' WOOF WOOF');
+// 	} else {
+// 		console.log(name + ' woof woof');
+// 	}
+// }
+// bark('barsik', 22);
+// bark('sharik', 10);
+
+
+// function whatShallWear(temp) {
+// 	if (temp < 60) {
+// 		console.log('Wear a jacket');
+// 	} else if (temp < 70) {
+// 		console.log('Wear a sweater');
+// 	} else {
+// 		console.log('Wear t-shirt');
+// 	}
+// }
+// whatShallWear(60);
+// whatShallWear(50);
+// whatShallWear(80);
+
+
+// function summa(arr) {
+// 	let sum = 0;
+// 	for (let i = 0; i < arr.length; i++) {
+// 		sum += arr[i];
+// 	console.log(sum);
+// 	}
+// }
+// let array = [5, 7, 35];
+
+// summa(array);
